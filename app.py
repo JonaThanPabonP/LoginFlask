de76ed5bbe9bc256001e6b24b802233cd0a7f209
JonaThanPabonP <jpaco95@gmail.com>
2023-03-31 19:31:07 -0500

    Función Login y Register. Carpetas templates y static.

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit()
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrarionForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/')
-def index():
-    return "Hola"
-
+def home():
+    return "Bienvenido a la página de inicio"
 
 if __name__ == '__main__':
     app.run()
+
---
+++ config.py
@@ -0,0 +1,4 @@
+
+class Config:
+    SECRET_KEY = '4c76f6efcff42c3f92ae2e73325ee002'
+    Debug = True
